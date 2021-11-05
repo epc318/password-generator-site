fff49659a07bdb21a7dd21a1dd53fbffe0e0688d
Rick Johnson <carl4917@umn.edu>
2021-11-05 18:26:09 -0500

    more prompts

--- Develop/script.js
+++ Develop/script.js
@@ -6,32 +6,46 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("we click it cuh");
 
-  //1.) prompt the user for password criteria
+  //1. prompt the user for password criteria
   var promptStart = window.confirm('Would You Like Generate a password?')
       
      // if user picks "cancel", confirm and refresh page to start
       if (promptStart === false) {
         var confirmSkip = window.confirm("Are you sure?")
         if (confirmSkip) {
-          return "Bye Bye";
-          break;
+          return "Bye Bye!";
         }
+      }
+
+     // a. password length between8 and 128
+  var promptLength = window.prompt('How many charaters would you like your password to be? (Must be 7 < 129)')
+
+      while (promptStart === true) {
+        
+        if (promptLength >= 129 || promptLength <= 7) {
+      window.alert("Invalid number! Please enter another.");
+      }
+
+      if (promptLength < 129 || promptLength > 7) {
+        promptLength = (this) 
+      }
+      break;
     }
-
-      
-    // if user picks "ok," continue to next prompt
-    if (promptStart === true)
-
     
 
+  // b. include lowercase, uppercase, numeric, or special characters?
+  var promptLower = windowconfirm('Would you like to include lowercase letters?')
+   
+  var promptUpper = windowconfirm('Would you like to include uppercase letters?')
 
+  var promptNumeric = windowconfirm('Would you like to include numeric values?')
+
+  var promptSpecial = windowconfirm('Would you like to include special characters?')
   
-
-
-  // a. password length between8 and 128
-  // b. include lowercase, uppercase, numeric, or special characters.
-
-
+  
+  
+  
+  
   //2.) validate input
   //3. generate password based on criteria
   
