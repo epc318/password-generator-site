f1c06e9d9032228efcdc1e3687acf5dddd2c3cab
Rick Johnson <carl4917@umn.edu>
2021-11-06 11:56:03 -0500

    cleanning up code and adding inclusion prompts

--- Develop/script.js
+++ Develop/script.js
@@ -6,62 +6,57 @@ var generateBtn = document.querySelector("#generate");
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
-var specialChar = "!@,#$%&*{}[]/\\+=";
+var specialChar = "!@,#$%&*=|-{}_();:?~`[]/\\+";
 var chosenCharacters = "";
 
 
 // creating/defining generatePassword function
 function generatePassword() {
 //Start user prompt process/get the necessary dasta for random gen
-var result = "";
+var endResult = "";
 
-  //1. prompt the user for password criteria
-  var promptStart = window.confirm('Would You Like Generate a password?')
-      
-     // if user picks "cancel", confirm and refresh page to start
-      if (promptStart === false) {
-        var confirmSkip = window.confirm("Are you sure?")
-        if (confirmSkip) {
-          return "Bye Bye!";
-        }
-      }
+//1. prompt the user for password criteria
+var promptStart = window.confirm('Would You Like Generate a password?')
 
-     // a. password length between8 and 128
-  
-     var promptLength = prompt("How many characters in your random password? (between 8 and 128)");
-     if(isNaN(promptLength)){
-       alert("You must input a number!");
-       return generatePassword()
-     }
-     if(promptLength<8 || promptLength> 128){
-       alert("Invalid number! Please choose numbers between 8 - 128.");
-       return generatePassword()
-     }
+  // if user picks "cancel", confirm and refresh page to start
+  if (promptStart === false) {
+    var confirmSkip = window.confirm("Are you sure?")
+  if (confirmSkip) {
+  return "Bye Bye!";
+  }
+ } 
+
+// a. password length between8 and 128
+var promptLength = prompt("How many characters in your random password? (Must be 8 - 128)");
+  if(isNaN(promptLength)){
+    alert("You must input a number!");
+  return generatePassword()
+  }
+  if(promptLength<8 || promptLength> 128){
+    alert("Invalid number! Please choose numbers between 8 - 128.");
+  return generatePassword()
+  }
     
+// b. include lowercase, uppercase, numeric, or special characters?
+var promptUpper = confirm("Would you like to include uppercase letters?");
+var promptLower = confirm("Would you like to include lowercase letters?");
+var promptNumbers = confirm("Would you like to include numerical values?");
+var promptSpecial = confirm("Would you like to include any special characters?");
 
-  // b. include lowercase, uppercase, numeric, or special characters?
- // var promptLower = windowconfirm('Would you like to include lowercase letters?')
+//2.) validate input
+  if(!promptUpper&&!promptLower&&!promptNumbers&&!promptSpecial){
+    alert("You must at least choose 1 character type!");
+  return confirm("Would you like to include uppercase letters?")
+  }
 
-     // if (promptLower === false)
-   
-//  var promptUpper = windowconfirm('Would you like to include uppercase letters?')
 
-     // if (promptUpper === false)
 
- // var promptNumeric = windowconfirm('Would you like to include numeric values?')
 
-  //var promptSpecial = windowconfirm('Would you like to include special characters?')
-  
-  
-  
-  
-  
-  //2.) validate input
   //3. generate password based on criteria
   
   
   //4.) display password on page (return)
-  return "genreated password will go here";
+  return result;
 }
 
 
